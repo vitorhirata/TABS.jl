264aa3751f4391bcd4511e78e2aa1b32f0022fce
Vitor Hirata Sanches <vitors.hirata@gmail.com>
2023-10-02 02:49:39 -0300

    Implement Community struct creation
    Based on id, matrix of resources and varieties it creates the community
    grid. This function will be used in the model setup.

--- src/entities/community.jl
+++ src/entities/community.jl
@@ -4,11 +4,19 @@ mutable struct Community
 end
 
 """
-    Community(id::Int)
+    Community(id::Int, resources::Matrix{Tuple{Float64, Float64, Float64}}, varieties::Matrix{Variety})
 
-Create community based on id.
+Create community based on id, matrix of resources and varieties.
 """
-# TODO: implement plot_fractions setup
-function Community(id::Int)
-    Community(id, [;;])
+function Community(id::Int, resources::Matrix{Tuple{Float64, Float64, Float64}}, varieties::Matrix{Variety})
+    lattice_size = trunc(Int, sqrt(length(varieties)))
+    grid = Matrix{PlotFraction}(undef, lattice_size, lattice_size)
+
+    for row in 1:lattice_size
+        for col in 1:lattice_size
+            grid[row, col] = PlotFraction(resources[row, col], varieties[row, col])
+        end
+    end
+
+    Community(id, grid)
 end
--- test/entities/community.jl
+++ test/entities/community.jl
@@ -1,10 +1,20 @@
 @testset "Community creation" begin
     @testset "Community creation with input" begin
         id = 10
+        lattice_size = 49
 
-        community = TABS.Community(id)
+        resource = (0.1, 0.2, 0.3)
+        resources = fill(resource, (lattice_size, lattice_size))
+        variety = TABS.Variety()
+        varieties = fill(variety, (lattice_size, lattice_size))
+
+        community = TABS.Community(id, resources, varieties)
 
         @test community.id == id
-        @test community.grid == [;;]
+        @test length(community.grid) == lattice_size^2
+        @test community.grid[1, 1].resource == resource
+        @test community.grid[1, 1].variety == variety
+        @test community.grid[40, 40].resource == resource
+        @test community.grid[40, 40].variety == variety
     end
 end
